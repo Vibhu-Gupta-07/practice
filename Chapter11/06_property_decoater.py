f0bab5b2deed263c2ec439882eb9153a645ba4c9
alphaengineer2205 <156074220+alphaengineer2205@users.noreply.github.com>
2024-08-25 12:30:39 +0530

    main

--- Chapter11/06_property_decoater.py
+++ Chapter11/06_property_decoater.py
@@ -5,10 +5,19 @@ class employee:
     def show(cls):
         print(f"The class value of a is {cls.a}")
 
+    @property
+    def name(self):
+        return f"{self.fname} {self.lname}"
+    
+    @name.setter
+    def name(self,value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
 e = employee()
 e.a = 45
 
-e.name = "Harry"
-print(e.name)
+e.name = "vibhu gupta"
+print(e.fname, e.lname)
 
 e.show()
